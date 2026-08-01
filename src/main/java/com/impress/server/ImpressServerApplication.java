614f6399668aed5402f1c46a7ebac6259322b948
soohyungit <soohyun5576@gmail.com>
2026-08-01 19:33:00 +0900

    ci: GitHub Actions EC2 자동 배포 스크립트 추가)
    * build: Spring Boot 프로젝트 초기 설정
    
    * ci: GitHub Actions EC2 자동 배포 스크립트 추가
    
    ---------
    
    Co-authored-by: YJ-Y <75804468+YJ-Y@users.noreply.github.com>

---
+++ src/main/java/com/impress/server/ImpressServerApplication.java
@@ -0,0 +1,13 @@
+package com.impress.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpressServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpressServerApplication.class, args);
+	}
+
+}
