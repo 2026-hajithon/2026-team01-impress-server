83465ad3a757ecf56bd77d8a14f76420b43738ad
YJ-Y <75804468+YJ-Y@users.noreply.github.com>
2026-08-01 19:14:58 +0900

    build: Spring Boot 프로젝트 초기 설정

---
+++ src/test/java/com/impress/server/ImpressServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impress.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpressServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
