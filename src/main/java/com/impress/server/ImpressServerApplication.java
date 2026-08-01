83c58ae81d2aebb677d46a724b734e2afac37057
soohyungit <soohyun5576@gmail.com>
2026-08-01 20:45:50 +0900

    feat: rds DB 연결

--- src/main/java/com/impress/server/ImpressServerApplication.java
+++ src/main/java/com/impress/server/ImpressServerApplication.java
@@ -2,9 +2,8 @@ package com.impress.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class ImpressServerApplication {
 
 	public static void main(String[] args) {
